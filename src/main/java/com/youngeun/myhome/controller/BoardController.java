945d22a60b6e4801f8b095587dba189dc6e1e3b2
choyoungeun <youngcool0.0@gmail.com>
2021-10-08 03:29:13 +0900

    pagination,search,RESTful API,...

--- src/main/java/com/youngeun/myhome/controller/BoardController.java
+++ src/main/java/com/youngeun/myhome/controller/BoardController.java
@@ -2,12 +2,18 @@ package com.youngeun.myhome.controller;
 
 import com.youngeun.myhome.model.Board;
 import com.youngeun.myhome.repository.BoardRepository;
+import com.youngeun.myhome.validator.BoardValidator;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.*;
 
+import javax.validation.Valid;
 import java.util.List;
 
 @Controller
@@ -15,16 +21,40 @@ public class BoardController {
 
     @Autowired
     private BoardRepository boardRepository;
+    @Autowired
+    private BoardValidator boardValidator;
 
     @GetMapping("/list")
-    public String list(Model model){
-        List<Board> boards = boardRepository.findAll();
+    public String list(Model model, @PageableDefault(size = 2)Pageable pageable, @RequestParam(required = false,defaultValue = "") String searchText){
+        Page<Board> boards = boardRepository.findByTitleContainingOrContentContainingOrNameContaining(searchText,searchText,searchText,pageable);
+        int startPage = Math.max(1,boards.getPageable().getPageNumber()-4);
+        int endPage = Math.min(boards.getTotalPages(),boards.getPageable().getPageNumber()+4);
+        model.addAttribute("startPage",startPage);
+        model.addAttribute("endPage",endPage);
         model.addAttribute("boards",boards);
         return "list";
     }
 
     @GetMapping("/form")
-    public String form(Model model){
+    public String form(Model model, @RequestParam(required = false) Long id){
+        if (id == null){
+            model.addAttribute("board",new Board());
+        }
+        else{
+            Board board = boardRepository.findById(id).orElse(null);
+            model.addAttribute("board",board);
+        }
         return "form";
     }
+
+    @PostMapping("/form")
+    public String greetingSubmit(@Valid Board board, BindingResult bindingResult){
+        boardValidator.validate(board, bindingResult);
+        if(bindingResult.hasErrors()){
+            return "form";
+        }
+        boardRepository.save(board);
+        return "redirect:/list";
+    }
+
 }
--- src/main/java/com/youngeun/myhome/model/Board.java
+++ src/main/java/com/youngeun/myhome/model/Board.java
@@ -6,6 +6,9 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
 
 @Entity
 @Data
@@ -13,6 +16,10 @@ public class Board {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
+    @NotEmpty(message = "이름을 입력해주세요.")
+    private String name;
+    @NotNull
+    @Size(min=2, max=30, message = "2자이상 30자 이하로 작성해주세요.")
     private String title;
     private String content;
 }
